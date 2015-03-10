eec6e1231af75385e8b91a85ef9ac43affdef5d4
madhaanr <marko.haanranta@helsinki.fi>
2015-03-10 15:21:02 +0200

    w3 more

--- app/controllers/askare_controller.php
+++ app/controllers/askare_controller.php
@@ -3,6 +3,6 @@
 class AskareController extends BaseController {
     public static function index() {
         $askareet=Askare::all();
-        View::make('askarelista.html', array('askareet' => $askareet));
+        View::make('askarelista.html.twig', array('askareet' => $askareet));
     }
 }
--- app/models/askare.php
+++ app/models/askare.php
@@ -1,5 +1,28 @@
 <?php
 
 class Askare extends BaseModel{
+    public $id, $nimi, $tarkeys, $luokka, $kuvaus, $lisatty, $kuka;
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
     
+    public static function all() {
+        $query = DB::connection()->prepare('SELECT * FROM ASKARE');
+        $query->execute();
+        $rows = $query->fetchAll();
+        $askareet = array();
+        
+        foreach($rows as $row) {
+            $askareet[] = new Askare(array(
+               'id'=>$row['id'],
+               'nimi'=>$row['nimi'],
+               'tarkeys'=>$row['$tarkeys'],
+               'luokka'=>$row['luokka'],
+               'kuvaus'=>$row['kuvaus'],
+               'lisatty'=>$row['lisatty'],
+               'kuka'=>$row['kuka']
+            )); 
+        }
+        return $askareet;
+    }
 }
