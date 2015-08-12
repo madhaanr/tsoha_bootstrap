6628df6cd3c8b9a6e521d23ded4c87041b5639b2
madhaanr <marko.haanranta@helsinki.fi>
2015-08-12 14:48:14 +0300

    edit, update, destroy under work

--- app/controllers/askare_controller.php
+++ app/controllers/askare_controller.php
@@ -3,7 +3,7 @@
 class AskareController extends BaseController {
 
     public static function create() {
-        View::make('askare/new.html.twig');  
+        View::make('askare/new.html.twig');
     }
 
     public static function index() {
@@ -18,19 +18,45 @@ class AskareController extends BaseController {
 
     public static function store() {
         $params = $_POST;
-        $attributes = new Askare(array(
+        $askare = new Askare(array(
             'nimi' => $params['nimi'],
             'tarkeys' => $params['tarkeys'],
             'luokka' => $params['luokka'],
             'kuvaus' => $params['kuvaus']
         ));
-        $askare = new Askare($attributes);
+//        $askare = new Askare($attributes);
         $errors = $askare->errors();
         if (count($errors) == 0) {
             $askare->save();
             Redirect::to('/askare/' . $askare->id, array('message' => 'Askare lisätty!'));
         } else {
-            View::make('askare/new.html.twig', array('errors' => $errors, 'attributes' => $attributes));
+            View::make('askare/new.html.twig', array('errors' => $errors, 'askare' => $askare));
+        }
+    }
+
+    public static function edit($id) {
+        $askare = Askare::find($id);
+        View::make('askare/edit.html.twig', array('attributes' => $askare));
+    }
+
+    public static function update($id) {
+        $params = $_POST;
+
+        $askare = new Askare(array(
+            'nimi' => $params['nimi'],
+            'tarkeys' => $params['tarkeys'],
+            'luokka' => $params['luokka'],
+            'kuvaus' => $params['kuvaus'],
+            'lisatty' => $params['lisatty']
+        ));
+
+        $errors = $askare->errors();
+        if(count($errors)==0) {
+            $askare->update();
+            Redirect::to('/askare/'.$askare->id,array('message'=>'Askaretta muokattu!'));
+        }
+        else {
+            View::make('askare/edit.html.twig',array('errors'=>$errors, 'askare'=>$askare));
         }
     }
 
